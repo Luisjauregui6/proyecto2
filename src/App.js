49ed0ea773a0c7335632242c407e3bad670de2cf
Luis jauregui <Luisjauregui221@gmail.com>
2022-02-11 16:28:00 -0600

    version 1

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Días no disponibles",
+        allDay: true,
+        start: new Date(2022, 6, 0),
+        end: new Date(2022, 6, 0),
+    },
+    {
+        title: "Días no disponibles",
+        start: new Date(2022, 1, 8),
+        end: new Date(2022, 1, 15),
+    },
+    {
+        title: "Días disponibles",
+        start: new Date(2022, 1, 3),
+        end: new Date(2022, 1, 8),
+    },
+];
+
+function App() {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent]);
+    }
+
+    return (
+        <div className="App">
+            <h1>Calendario</h1>
+            <h2>Agregar Nuevo Evento</h2>
+            <div>
+                <input type="text" placeholder="Agregar titulo" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Inicio" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="Fin" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "15px" }} onClick={handleAddEvent}>
+                    Agregar Evento
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        </div>
+    );
+}
+
+export default App;
